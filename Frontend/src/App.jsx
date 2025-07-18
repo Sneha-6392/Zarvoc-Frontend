97467b49bde8a5d15d9b04b4f7f326537b804fd0
AJ-00123 <arpanjaincodex@gmail.com>
2025-07-18 17:57:27 +0530

    Profile page + backend added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './index.css';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 
+
 import LandingPage from './Pages/LandingPage.jsx';
 import Category from './Pages/Category.jsx';
 import ContactUs from './Pages/ContactUs.jsx';
@@ -16,7 +17,9 @@ import SecureCheckout from './Pages/SecureCheckout.jsx';
 import WelcomePage from './Pages/WelcomePage.jsx';
 import Login from './Pages/Login.jsx';
 import AboutUs from './Pages/AboutUs.jsx';
-
+import BrandMarquee from './Pages/BrandMarquee.jsx';
+import EditProfile from './Pages/editprofile.jsx';
+import ProfilePage from './Pages/profilepage.jsx';
 function ScrollToTop() {
   const { pathname } = useLocation();
 
@@ -31,10 +34,10 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <ScrollToTop /> {/* ✅ Scroll fix added here */}
+        <ScrollToTop /> 
         <Routes>
           <Route path="/category" element={<Category />} />
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage />} /> 
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/cartpage" element={<CartPage />} />
           <Route path="/trackorder" element={<TrackOrder />} />
@@ -46,6 +49,13 @@ function App() {
           <Route path="/checkout" element={<SecureCheckout />} />
           <Route path="/welcomepage" element={<WelcomePage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/marque" element={<BrandMarquee />} />
+          <Route path="/profile" element={<ProfilePage />} />  
+          <Route path="/editprofile" element={<EditProfile />} /> 
+          
+
+          
+
         </Routes>
       </BrowserRouter>
     </div>
